2c631083aafb9a306a395c53c46aea1cfd3e1aec
David Funnell <dfunnell77@gmail.com>
2023-05-18 17:23:51 -0700

    added MUIv5 w/ related dependencies to package.json. Added a theme.ts file and imported it to _app.tsx. Updated index by removing boileplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import theme from '../styles/theme'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
